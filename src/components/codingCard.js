74f0a7f5e9a64f256408237d76650ae17917e15d
Larkin <larkindsmith@gmail.com>
2021-06-17 17:05:11 -0400

    Coding Cards: links added and styled

--- src/components/codingCard.js
+++ src/components/codingCard.js
@@ -10,6 +10,20 @@ export default function CodingCard({ project }) {
         </div>
         <p className="projectStack">{project.stack}</p>
         <p className="projectInfo">{project.info}</p>
+
+        <div className="projectLinks">
+          {project.website ? (
+            <a className="singleLink" href={project.website}>
+              Website
+            </a>
+          ) : (
+            <></>
+          )}
+
+          <a className="singleLink" href={project.github}>
+            Github
+          </a>
+        </div>
       </div>
     </div>
   );
